3fe11a3fb4d0baf7c7172903b5225793f4ae26a5
Anna Vladimirova <annafreia@mail.ru>
2017-08-26 17:37:41 +0300

    added embedded jetty

---
+++ src/main/java/simplechat/init/EmbeddedJetty.java
@@ -0,0 +1,47 @@
+package simplechat.init;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.IOException;
+
+public class EmbeddedJetty {
+
+    private static final int DEFAULT_PORT = 8080;
+    private static final String CONTEXT_PATH = "/";
+    private static final String CONFIG_LOCATION = "simplechat.config";
+    private static final String MAPPING_URL = "/*";
+
+    public static void main(String[] args) throws Exception {
+        new EmbeddedJetty().startJetty(DEFAULT_PORT);
+    }
+
+    private void startJetty(int port) throws Exception {
+        Server server = new Server(port);
+        server.setHandler(getServletContextHandler(getContext()));
+        server.start();
+        server.join();
+    }
+
+    private static ServletContextHandler getServletContextHandler(WebApplicationContext context) throws IOException {
+        ServletContextHandler contextHandler = new ServletContextHandler();
+        contextHandler.setErrorHandler(null);
+        contextHandler.setContextPath(CONTEXT_PATH);
+        contextHandler.addServlet(new ServletHolder(new DispatcherServlet(context)), MAPPING_URL);
+        contextHandler.addEventListener(new ContextLoaderListener(context));
+        contextHandler.setResourceBase(new ClassPathResource("webapp").getURI().toString());
+        return contextHandler;
+    }
+
+    private static WebApplicationContext getContext() {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.setConfigLocation(CONFIG_LOCATION);
+        return context;
+    }
+}
